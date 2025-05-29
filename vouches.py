bff1de56ee8a7eed6973bc6ea9a1ef3055e28191
kacemMathlouthi442 <wmathlouthi661@gmail.com>
2025-05-29 02:15:30 +0100

    Update vouches.py

--- vouches.py
+++ vouches.py
@@ -18,26 +18,20 @@ bot = Bot(os.environ.get('token'))
 lengt = [4,6,8]
 async def main_loop():
     while True:
-        try:
-            otp = ''.join([str(randint(0, 9)) for _ in range(lengt[randint(0,2)])])
-            service = services[randint(0, len(services)-1)]
-            msg1 = (
-                f"<b>📲 AORUS OTP 📲 ┃ VOUCHES </b>\n"
-                f"➖➖➖➖➖➖\n"
-                f"╭  Service Name ➣ <b>{service}</b>\n"
-                f"⭐ OTP ➣ <code>{otp}</code> ✅\n"
-                f"╰  Capture By: -----------\n"
-                f"🤖 <a href='https://t.me/AORUS_OTP_bot'>BOT</a>"
-            )
-            await bot.send_message(chat_id=-1002662428684, text=msg1, parse_mode='HTML')
-        except Exception as e:
-            print(f"❌ Error sending to channel 1: {e}")
+        otp = ''.join([str(randint(0, 9)) for _ in range(lengt[randint(0,2)])])
+        service = services[randint(0, len(services)-1)]
+        msg1 = (
+            f"<b>📲 AORUS OTP 📲 ┃ VOUCHES </b>\n"
+            f"➖➖➖➖➖➖\n"
+            f"╭  Service Name ➣ <b>{service}</b>\n"
+            f"⭐ OTP ➣ <code>{otp}</code> ✅\n"
+            f"╰  Capture By: -----------\n"
+            f"🤖 <a href='https://t.me/AORUS_OTP_bot'>BOT</a>"
+        )
+        await bot.send_message(chat_id=-1002662428684, text=msg1, parse_mode='HTML')
 
         await asyncio.sleep(randint(3, 9))
 
 
 if __name__ == "__main__":
-    try:
-        asyncio.run(main_loop())
-    except KeyboardInterrupt:
-        print("Stopped by user.")
+    asyncio.run(main_loop())
